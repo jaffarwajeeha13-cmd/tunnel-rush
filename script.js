a985202c4968f80950a5d9bda4aa707d518211f5
narjis avd <avdnarjis@gmail.com>
2026-03-30 18:25:23 +0500

    MAI N WEBSITE

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const nextbutton = document.querySelector(".next-btn");
 const video = document.querySelector(".hero-video");
-const movielist=["videos/hero-1.mp4", "videos/hero-2.mp4", "videos/hero-3.mp4", "videos/hero-4.mp4"]; 
+const movielist=["videos/feature-1.mp4", "videos/feature-3.mp4", "videos/feature-2.mp4", "videos/hero-3.mp4"]; 
 
 let index = 0;
 nextbutton.addEventListener("click", function(){
